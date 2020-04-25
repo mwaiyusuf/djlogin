202bc6f876c3cf656ed6adfd4b3fbb4fc74e70fb
Mwaiyusuf <mwaigalo5@gmail.com>
2020-04-25 19:51:26 +0300

    name Error

--- login/forms.py
+++ login/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from dappx.models import UserProfileInfo
+from login.models import UserProfileInfo
 from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
