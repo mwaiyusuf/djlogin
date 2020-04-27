343afa26a1ffa57743d27813a272ba6347c709a3
Mwaiyusuf <mwaigalo5@gmail.com>
2020-04-27 21:56:20 +0300

    nameErrror

--- login/admin.py
+++ login/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 # from django.contrib import admin
-from dappx.models import UserProfileInfo, User
+from login.models import UserProfileInfo, User
 # Register your models here.
 admin.site.register(UserProfileInfo)
