290261c72f91bee66892defcbe14943ba23a455f
Mwaiyusuf <mwaigalo5@gmail.com>
2020-04-27 22:02:31 +0300

    nameErrror

--- login/views.py
+++ login/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 # from django.shortcuts import render
-from dappx.forms import UserForm,UserProfileInfoForm
+from login.forms import UserForm,UserProfileInfoForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 def index(request):
-    return render(request,'dappx/index.html')
+    return render(request,'login/index.html')
 @login_required
 def special(request):
     return HttpResponse("You are logged in !")
