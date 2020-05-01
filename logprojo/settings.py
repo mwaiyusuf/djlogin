f2429d0a47771a42b4f0091645f18e645705c22b
Mwaiyusuf <mwaigalo5@gmail.com>
2020-05-01 21:32:16 +0300

    indentation Error

--- logprojo/settings.py
+++ logprojo/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+STATICFILES_DIRS = [ 
+     os.path.join(BASE_DIR, "static"),
+]
 
-MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL =  ‘/media/’
 
 # LOGIN_URL = ‘/login/user_login/’
